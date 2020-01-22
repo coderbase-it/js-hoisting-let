a46ee1032c46d9ad815da5b5ff5b4396a23faf4d
coderbase-it <coderbaseit@gmail.com>
2020-01-22 19:37:48 +0100

    1 file modified

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 function getName(user) {
   if (user.isAdmin) {
-    var name = "Admin : " + user.name;
+    var name = 'Admin : ' + user.name;
     return name;
   }
-  console.log(name)
+  //console.log(name)
   return user.name;
 }
 const res = getName({name: 'Pierre', isAdmin:false})
-console.log(res)
+//console.log(res)
+
+
+ const user  = { name : 'Angular', version : 9};
+ const { name : finalName, version : angularVersion } = user 
+ console.log(finalName)
+  console.log(angularVersion)
+
+ const user  = { name : 'Angular', version : 9};
+ const { name , version } = user 
+  console.log(name)
+  console.log(version)
